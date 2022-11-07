05a58a315e6fd6a38ad010ab0a6c8811015d0fdf
Dave Braun <dab414@lehigh.edu>
2022-11-07 08:55:54 -0500

    Remove upload script

--- upload.sh
+++
@@ -1,4 +0,0 @@
-pandoc index.md -o index.html --template=bootstrap_menu.html --toc
-
-rsync -avP --delete -e 'ssh -p 18765'  /home/dave/Dropbox\ \(Lehigh\ University\)/post_doc/professional/learning/math/ u231-f6hnzkbwb44b@davebraun.net:~/www/davebraun.net/public_html/math/
-
